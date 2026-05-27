80dc1d2f53ddac13ef2bc84f73a60d282d835fb4
Subharthi Dutta <133211864+subhaiml@users.noreply.github.com>
2026-05-27 19:18:24 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,13 @@
+
+
+class Solution {
+    public static boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
